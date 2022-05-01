8dc0175f7fb8bc8845253dc2f74cea619398fa38
ChristianMendoza3115 <104573632+ChristianMendoza3115@users.noreply.github.com>
2022-04-30 20:14:22 -0500

    commit

--- src/main/kotlin/operadores.kt
+++ src/main/kotlin/operadores.kt
@@ -1,3 +1,8 @@
+//OPERADORES ARITMETICOS
 fun main(){
-    println("Hola")
+    var num1=10
+    var num2=8
+
+    println("Hola Christian, esto es una operacion matematica")
+    println("La suma de $num1 y $num2 es: ${num1+num2}")
 }
