d6dd7de582a44bb95f04c51f1cb5f7b8acc84282
ChristianMendoza3115 <xkikin@gmail.com>
2022-05-06 20:44:40 -0500

    commit

---
+++ src/main/kotlin/Clase_DesarrolloDeAplicaciones/EjerciciosPracticasSolo/Ejercicio1.kt
@@ -0,0 +1,8 @@
+//Pedir datos al usuario y si es que es mayor de edad, pedirle dni y preguntar si trabaja, si es asi preguntar el sueldo si fana mas de
+//1000 descontarle 10% de impuesto
+fun main(){
+
+
+
+
+}
